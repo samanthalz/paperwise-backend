7f8ac546874266a7067bd2654f268f25b1a8922b
samanthalz <samanthalz1610@gmail.com>
2025-11-03 12:44:33 +0800

    https setup

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app = FastAPI(title="PDF RAG Pipeline")
 # --- CORS ---
 origins = [
     "http://localhost:3000",
+    "https://localhost:3000",
     "http://127.0.0.1:3000",
     "https://nextjs-fyp-tau.vercel.app",
 ]
